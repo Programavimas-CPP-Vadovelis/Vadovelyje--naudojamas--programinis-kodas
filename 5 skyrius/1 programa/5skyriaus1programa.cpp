0761c843f943aab8bcb897764ab244cc15b40475
Rimantas Belevičius & Saulius Valentinavičius <108738107+Programavimas-CPP-Vadovelis@users.noreply.github.com>
2022-07-14 15:06:15 +0300

    Create 5skyriaus1programa.cpp

---
+++ 5 skyrius/1 programa/5skyriaus1programa.cpp
@@ -0,0 +1,23 @@
+import <iostream>;
+using std::cout;
+using std::endl;
+//
+void stars();   // funkcijos prototipas
+//
+int main() {
+	//
+	stars();   // kviečiama funkcija pirmajai eilutei išvesti
+	cout << "Duomens tipas     Ilgis     Diapazonas\n";
+	stars();  // eilutė lentelės antraštei atriboti
+	cout << "char             1B       -128 - +127" << endl
+<< "short            2B       -32768 - +32767" << endl
+<< "int              4B       ~-2e9 - ~+2e9" << endl
+<< "long             4B       ~-2e9 - ~+2e9" << endl;
+	stars();  // eilutė lentelei užbaigti
+	//
+}
+//
+void stars() {       // funkcijos kūnas
+	for (size_t i = 0; i < 45; i++) cout << '*';
+	cout << endl;
+}
