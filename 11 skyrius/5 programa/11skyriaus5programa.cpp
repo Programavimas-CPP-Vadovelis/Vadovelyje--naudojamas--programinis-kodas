8772553d3cfeddcfb92fdc3729f90dcd25926035
Rimantas Belevičius & Saulius Valentinavičius <108738107+Programavimas-CPP-Vadovelis@users.noreply.github.com>
2022-07-14 17:34:05 +0300

    Create 11skyriaus5programa.cpp

---
+++ 11 skyrius/5 programa/11skyriaus5programa.cpp
@@ -0,0 +1,53 @@
+import <iostream>;
+using std::cout;
+using std::cin;
+using std::endl;
+//
+class Distance {
+private:
+	int feet;       // pėdos
+	double inches;  // coliai
+	//
+public:
+	Distance() : feet(0), inches(0.) { }
+	Distance(int ft, double in) : feet(ft), inches(in) { }
+	// 
+	~Distance() { }
+	// 
+public:
+	void getDistance() {
+		cout << "Iveskite pedas ir colius\n";
+		cin >> feet >> inches;
+	}
+	void showDistance() {
+cout << "Pedos: " << feet << "  coliai: " << inches << endl;
+	}
+	//
+	void addDistances(Distance d1, Distance d2) {
+		inches = d1.inches + d2.inches;
+		feet = d1.feet + d2.feet;
+		if (inches >= 12.) {
+			inches -= 12.;
+			feet++;
+		}
+	}
+	//
+};
+//
+int main() {
+	//
+	Distance d1, d2;
+	Distance d3(1, 2.3);
+	//
+	d2.getDistance();
+	//
+	cout << "d1: ";    d1.showDistance();
+	cout << "d2: ";    d2.showDistance();
+	cout << "d3: ";    d3.showDistance();
+	//
+	d1.addDistances(d2, d3);
+	//
+	cout << "d1: ";    d1.showDistance();
+	//
+	cout << endl;
+}
