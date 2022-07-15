3dde1d1b37d97b2b0a95873312b5140b9c73fd5d
Rimantas Belevičius & Saulius Valentinavičius <108738107+Programavimas-CPP-Vadovelis@users.noreply.github.com>
2022-07-15 16:24:23 +0300

    Update 13skyriaus2programa.cpp

--- 13 skyrius/2 programa/13skyriaus2programa.cpp
+++ 13 skyrius/2 programa/13skyriaus2programa.cpp
@@ -1,42 +1,55 @@
 import <iostream>;
+using std::cout;
+using std::endl;
 //
 class Counter {
-protected:                                                // 1 pastaba
+protected:
 	unsigned int count;
 public:
-	Counter() : count(0) { }       // konstruktorius
-	Counter(int c) : count(c) { }  // konstruktorius
+	Counter() : count(0) {
+		cout << "Bazines klases konstruktorius be argumentu\n";
+	}
+	Counter(int c) : count(c) {
+		cout << "Bazines klases konstruktorius su argumentais\n";
+	}
+	//
+	~Counter() { }
 	//
 	unsigned int returnCount() {
 		return count;
 	}
-	Counter operator++() {  // „++“ perkrovimas
+	Counter operator++() {
 		return Counter(++count);
 	}
 };
 //
-class CounterDown : public Counter {                     // 2 pastaba
+class CounterDown : public Counter {
 public:
-	Counter operator--() {  // „- -“ perkrovimas
-		return Counter(--count);
+	CounterDown() : Counter() {          // 1 pastaba
+		cout << "Isvestines klases konstruktorius be argumentu\n";
+	}
+	CounterDown(int c) : Counter(c) {   // 2 pastaba
+		cout << "Isvestines klases konstruktorius su argumentais\n";
+	}
+	//
+	~CounterDown() { }
+	//
+	CounterDown operator--() {
+		return CounterDown(--count);  // 3 pastaba
 	}
 };
 //
 int main() {
-	Counter c1;
-	CounterDown c2;                                       // 3 pastaba
+	CounterDown c1;
+	CounterDown c2{ 10 };
 	//
-	std::cout << "c1: " << c1.returnCount() << std::endl;
-	std::cout << "c2: " << c2.returnCount() << std::endl; // 4 pastaba
+	cout << "c1: " << c1.returnCount() << endl;
+	cout << "c2: " << c2.returnCount() << endl;
 	//
-	++c2; ++c2; ++c2;		                                  // 4 pastaba	
-	std::cout << "c2: " << c2.returnCount() << std::endl;
+	++c2; ++c2;
 	//
-	--c2; --c2;
-	std::cout << "c2: " << c2.returnCount() << std::endl;
-	//
-	++c1;
-	// --c1;                                              // 5 pastaba
-	std::cout << "c1: " << c1.returnCount() << std::endl;
+	CounterDown c3 = --c2;   // 4 pastaba
+	cout << "c2: " << c2.returnCount() << endl;
+	cout << "c3: " << c3.returnCount() << endl;
 	//
 }
