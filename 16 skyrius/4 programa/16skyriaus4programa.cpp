0c3b64e93fd0b509b6f962a110725e20603f2ab5
Rimantas Belevičius & Saulius Valentinavičius <108738107+Programavimas-CPP-Vadovelis@users.noreply.github.com>
2022-07-15 16:48:16 +0300

    Create 16skyriaus4programa.cpp

---
+++ 16 skyrius/4 programa/16skyriaus4programa.cpp
@@ -0,0 +1,18 @@
+import <iostream>;
+import <format>;
+using std::cout;
+using std::format;
+
+template <typename R=double, typename T1, typename T2>
+R larger(const T1& t1, const T2& t2) {
+	return static_cast<R> (t1 < t2 ? t2 : t1);
+}
+//
+int main() {
+	//
+	int i1{ 1 };
+	double d1{ 2. };
+	cout << format("Didesnis is  {} ir {} yra {:5.2f}\n",
+		i1, d1, larger(i1, d1));
+	//
+}
